98f12dc6acfba2f369b185f1184a7001aca060e9
Nilkesh konkani <konkani.nilkesh@gmail.com>
2022-10-29 12:11:49 +0530

    change in index

--- demoproject/demoapp/views.py
+++ demoproject/demoapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
 def index(request):
-    return JsonResponse("Success")
+    return JsonResponse("Success", safe=False)
